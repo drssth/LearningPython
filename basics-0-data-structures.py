3b44f931793ef2d4fa1515a1d72524c2690b220b
drssth <drssth@gmail.com>
2020-01-28 14:14:42 -0500

    edit

--- basics-0-data-structures.py
+++ basics-0-data-structures.py
@@ -6,18 +6,20 @@
 a,b,c,d,e = 17,17.5,"doesn't",[1,2,3],{"key":'value'}
 print(a,b,c,d,e)
 
+print("is 'a' an int?", isinstance(1,int))
+
 
 # class
 class Person(object):
     NAME = 'Person'
     ATTRIBUTES = ['gender', 'age', 'weight', 'height']
-    
+
     def __init__(self, name):
         self.name = name
-        
+
     def __call__(self):
         return self.name
-        
+
     def __lt__(self, other):
         return self.name < other.name
     def __le__(self, other):
@@ -30,32 +32,32 @@ class Person(object):
         return self.name > other.name
     def __ge__(self, other):
         return self.name >= other.name
-        
+
     def __str__(self):
         return '<Person(name={})>'.format(self.name)
-        
+
     def __enter__(self):
         print('enter object {}'.format(Person.NAME))
         return self
-        
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         print('exit object {} {}'.format(Person.NAME, self.name))
         del self.name
-        
+
     def __del__(self):
         print('delete object')
-        
-        
-        
-p1 = Person('Doris')        
+
+
+
+p1 = Person('Doris')
 p2 = Person('Mike')
 print(p1,p2, p1>p2)
 print(p1.NAME, p1.NAME, Person.NAME)
 
 with Person("David") as p:
     print(p, p.name)
-    
-    
+
+
 # list functions
 l = list(range(1,11))
 print(l)
